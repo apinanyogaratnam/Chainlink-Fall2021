f644c59f6124778450283a3c4d010534aaa0f53b
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-01 19:52:25 -0400

    completed connect button of metamask wallet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import { useEthers } from '@usedapp/core';
+import Button from '@material-ui/core/Button';
+
 export const Header = () => {
+    const { account, activateBrowserWallet } = useEthers();
+    const isConnected = account !== undefined;
+
     return (
-        <div>Hi from header</div>
+        <div>
+            {
+                isConnected ? 
+                <Button>Connected</Button> :
+                <Button onClick={activateBrowserWallet}>Connect</Button>
+            }
+        </div>
     );
 };
