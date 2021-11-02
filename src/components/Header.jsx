ed17cb584937fb262424cffb24f07adc0b4503cc
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-02 15:39:31 -0400

    displaying balance of token

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { useEthers } from '@usedapp/core';
+import { useEthers, useEtherBalance, useTokenBalance } from '@usedapp/core';
 import Button from '@material-ui/core/Button';
+import { formatEther, formatUnits } from '@ethersproject/units';
 
 export const Header = () => {
     const { account, activateBrowserWallet } = useEthers();
     const isConnected = account !== undefined;
+    const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
+    let tokenBalance = useTokenBalance(DAI, account);
+    tokenBalance = tokenBalance ? tokenBalance._hex : -1;
 
     return (
         <div>
@@ -16,18 +20,22 @@ export const Header = () => {
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 1 */}
                     <h1>CUBE</h1>
+                    <h4>Balance: {}</h4>
                 </div>
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 2 */}
                     <h1>SAND</h1>
+                    <h4>Balance: {}</h4>
                 </div>
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 3 */}
                     <h1>ETH</h1>
+                    <h4>Balance: {account ? tokenBalance : undefined}</h4>
                 </div>
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 4 */}
                     <h1>COIN</h1>
+                    <h4>Balance: {}</h4>
                 </div>
             </div>
         </div>
