80fddd1934c3a97b216698e83a668e6495488c6e
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-02 15:56:17 -0400

    displaying current amount of eth in metamask wallet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,17 @@ import { formatEther, formatUnits } from '@ethersproject/units';
 export const Header = () => {
     const { account, activateBrowserWallet } = useEthers();
     const isConnected = account !== undefined;
-    const DAI = '0x6b175474e89094c44da98b954eedeac495271d0f';
-    let tokenBalance = useTokenBalance(DAI, account);
-    tokenBalance = tokenBalance ? tokenBalance._hex : -1;
+    const SAND = '0x3845badAde8e6dFF049820680d1F14bD3903a5d0';
+    const CUBE = '0xdf801468a808a32656d2ed2d2d80b72a129739f4';
+
+    let sandBalance = useTokenBalance(SAND, account);
+    sandBalance = sandBalance ? sandBalance._hex : -1;
+
+    let ethBalance = useEtherBalance(account);
+    ethBalance = ethBalance ? ethBalance._hex : -1;
+
+    let cubeBalance = useTokenBalance(CUBE, account);
+    cubeBalance = cubeBalance ? cubeBalance._hex : -1;
 
     return (
         <div>
@@ -16,26 +24,27 @@ export const Header = () => {
                 <Button>Connected</Button> :
                 <Button onClick={activateBrowserWallet}>Connect</Button>
             }
+            <div>Balance: {formatEther(ethBalance)} ETH</div>
             <div className="coins-container">
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 1 */}
                     <h1>CUBE</h1>
-                    <h4>Balance: {}</h4>
+                    <h4>Balance: {-1}</h4>
                 </div>
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 2 */}
                     <h1>SAND</h1>
-                    <h4>Balance: {}</h4>
+                    <h4>Balance: {-1}</h4>
                 </div>
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 3 */}
-                    <h1>ETH</h1>
-                    <h4>Balance: {account ? tokenBalance : undefined}</h4>
+                    <h1>COIX</h1>
+                    <h4>Balance: {-1}</h4>
                 </div>
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 4 */}
                     <h1>COIN</h1>
-                    <h4>Balance: {}</h4>
+                    <h4>Balance: {-1}</h4>
                 </div>
             </div>
         </div>
