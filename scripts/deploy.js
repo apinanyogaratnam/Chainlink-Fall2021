9ab7a4bc862cc964d76afddadb0c95ba9f3cd3e9
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-02 20:47:05 -0400

    added json creation script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log(`Deploying contravts with the account: ${deployer.address}`);
@@ -8,6 +10,12 @@ async function main() {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
     console.log(`Greeter address: ${greeter.address}`);
+
+    const data = {
+        address: greeter.address,
+        abi: JSON.parse(greeter.interface.format('json'))
+    };
+    fs.writeFileSync('frontend/src/Greeter.json', JSON.stringify(data));
 }
 main()
     .then(() => process.exit(0))
