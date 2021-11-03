e1438ef1a2360b3a5a43a6c9ebe980ad832045a7
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-03 19:39:58 -0400

    removed previous code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { DAppProvider, ChainId } from '@usedapp/core';
 import { Header } from './components/Header';
-import getBlockchain from './ethereum.js';
-import { useState } from 'react';
 
 function App() {
-  const [greeter, setGreeter] = useState(undefined);
-
-  useEffect(() => {
-    const init = async () => {
-      const { obj } = await getBlockchain();
-      setGreeter(obj);
-    };
-    init();
-  }, []);
-
-  if (typeof greeter === 'undefined') {
-    return 'Loading...';
-  }
-
   return (
     <DAppProvider config={{
       supportedChains: [ChainId.Mainnet, ChainId.Localhost, ChainId.Rinkeby]
