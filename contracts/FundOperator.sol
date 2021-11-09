ccc4d2fc67134ab0daab632ad173ec0d1e486792
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-09 12:39:28 -0500

    completed add asset function

--- contracts/FundOperator.sol
+++ contracts/FundOperator.sol
@@ -20,16 +20,30 @@ contract FundOperator {
     constructor() {
     }
 
-    function checkIfTokenExists(address token) public view returns (bool) {
+    function checkIfTokenExists(address _token) public view returns (bool) {
         for (uint256 i = 0; i < isOperator.length; i++) {
-            if (isOperator[i].token.token == token) {
+            if (isOperator[i].token.token == _token) {
                 return true;
             }
         }
 
         return false;
     }
-    
+
+    function addAsset(string _name, address _token, uint256 _weighting) public onlyOwner{
+        require(_weighting > 0 && _weighting <= 100, "Weighting must be between 1 and 100");
+        require(!checkIfTokenExists(_token), "Token already exists");
+
+        Asset asset = Asset({
+            token: Token({
+                name: _name,
+                token: _token
+            }),
+            weighting: _weighting
+        });
+
+        isOperator[isOperator.length] = asset;
+    }
 
     // function addAsset(address token, string name, uint256 weighting) public onlyOwner{
     //     require(weighting > 0 && weighting <= 100);
