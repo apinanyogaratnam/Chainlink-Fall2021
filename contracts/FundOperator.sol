9525221e00301da8ef492f65dcc06e52370cc690
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-09 12:28:10 -0500

    created structs to store each token and the weighting of the token

--- contracts/FundOperator.sol
+++ contracts/FundOperator.sol
@@ -2,9 +2,19 @@ pragma solidity ^0.8.0;
 
 contract FundOperator {
 
-    string public name;
+    // make one giant struct to save on storage fees
+    struct Token {
+        string name;
+        address token;
+    }
+
+    struct Asset {
+        Token token;
+        uint256 weighting; // a number from 1 to 100
+    }
+    
+    mapping (uint256 => Asset) public isOperator;
 
     constructor() {
-        name = "FundOperator";
     }
 }
