3d765c598d9b501b52f786e768ba86bdef58cf5e
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-11 21:31:50 -0500

    reverted to web3 connection to metamask

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,41 +1,45 @@
-import { useEthers, useEtherBalance, useTokenBalance } from '@usedapp/core';
 import Button from '@material-ui/core/Button';
-import { formatEther, formatUnits } from '@ethersproject/units';
-import { GetGreeting } from './greetFunctions';
+import Web3 from 'web3';
+import { useState } from 'react';
 
 export const Header = () => {
-    const { account, activateBrowserWallet } = useEthers();
-    const isConnected = account !== undefined;
-    const SAND = '0x3845badAde8e6dFF049820680d1F14bD3903a5d0';
-    const CUBE = '0xdf801468a808a32656d2ed2d2d80b72a129739f4';
-    const MANA = '0x0f5d2fb29fb7d3cfee444a200298f468908cc942'
 
-    let sandBalance = useTokenBalance(SAND, account);
-    sandBalance = sandBalance ? sandBalance._hex : -1;
+    const [isConnected, setIsConnected] = useState(false);
+    const activateWeb3 = () => {
+        let provider = window.ethereum;
 
-    let ethBalance = useEtherBalance(account);
-    ethBalance = ethBalance ? ethBalance._hex : -1;
+        if (provider) {
+            provider
+                .request({
+                    method: 'eth_requestAccounts',
+                })
+                .then(accounts => {
+                    const selectedAccount = accounts[0];
+                    console.log("selected account is", selectedAccount);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            
+            window.ethereum.on('accountsChanged', function(accounts) {
+                const selectedAccount = accounts[0];
+                console.log("selected account is", selectedAccount);
+            });
 
-    let cubeBalance = useTokenBalance(CUBE, account);
-    cubeBalance = cubeBalance ? cubeBalance._hex : -1;
+            setIsConnected(true);
+        }
 
-    let manaBalance = useTokenBalance(MANA, account);
-    manaBalance = manaBalance ? manaBalance._hex : -1;
-
-    // formatUnits(sandBalance, 8);
-    // formatUnits(ethBalance, 8);
-    // formatUnits(cubeBalance, 8);
-    // formatUnits(manaBalance, 8);
-    // console.log(GetGreeting());
+        const web3 = new Web3(provider);
+    }
 
     return (
         <div>
             {
                 isConnected ? 
                 <Button>Connected</Button> :
-                <Button onClick={activateBrowserWallet}>Connect</Button>
+                <Button onClick={activateWeb3}>Connect</Button>
             }
-            <div>Balance: {formatEther(ethBalance)} ETH</div>
+            <div>Balance: {0} ETH</div>
             <div className="coins-container">
                 <div className="coin-container" onClick={() => console.log("asdf")}>
                     {/*Box 1 */}
