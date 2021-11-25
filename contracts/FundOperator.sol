eed26721c3626b32429b7cd98eeae6b2559a6fa7
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-25 08:44:19 -0500

    created remove asset and buy funds functions

--- contracts/FundOperator.sol
+++ contracts/FundOperator.sol
@@ -15,7 +15,6 @@ contract FundOperator is Ownable {
     constructor() {
     }
 
-// fix function so it compiles
     function getLengthOfMapping() public view returns (uint8) {
         uint8 count = 0;
         while (true) {
@@ -44,5 +43,45 @@ contract FundOperator is Ownable {
         require(_weighting >= 0 && _weighting <= 100, "Weighting must be between 0 and 100");
 
         tokens[getLengthOfMapping()] = Token(_token, _weighting);
-    }   
+    }
+
+    function removeAsset(address _token) public onlyOwner {
+        require(checkIfTokenExists(_token), "Token does not exist");
+
+        uint8 index = 0;
+        while (true) {
+            if (tokens[index].token == _token) {
+                break;
+            } else {
+                index++;
+            }
+        }
+
+        for (uint8 i = index; i < getLengthOfMapping() - 1; i++) {
+            tokens[i] = tokens[i + 1];
+        }
+
+        tokens[getLengthOfMapping() - 1] = Token(address(0), 0);
+    }
+
+    function buy(address _token, uint256 _amount) public payable {
+        require(checkIfTokenExists(_token), "Token does not exist");
+        require(_amount > 0, "Amount must be greater than 0");
+        require(msg.value == )
+
+        uint8 index = 0;
+        while (true) {
+            if (tokens[index].token == _token) {
+                break;
+            } else {
+                index++;
+            }
+        }
+
+        uint256 amount = _amount * tokens[index].weighting / 100;
+        //FIXME: This is not the correct way to do this
+        _token.transfer(msg.sender, amount);
+
+        // msg.sender.transfer(msg.value - amount);
+    }
 }
